94a490c3bcb8c0c6edb34905901a075bdbded493
haroundjudzman <haroun.djudzman@gmail.com>
2024-04-24 09:49:09 +0700

    feat(handler): add usecase interface and error handling

--- handler.go
+++ handler.go
@@ -1,17 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+
+	"ascenda-hotel/entity"
 )
 
-type Handler struct {
+const (
+	// ErrInvalidDestinationID is returned when the destination ID is invalid.
+	ErrInvalidDestinationID = "Invalid destination ID. Destination ID must be a non-negative integer."
+	// ErrInternalServerError is returned when an internal server error occurs.
+	ErrInternalServerError = "Internal server error. Please try again later or contact support."
+	// ErrNoHotelsFound is returned when no hotels are found.
+	ErrNoHotelsFound = "No hotels found."
+)
+
+type Usecase interface {
+	// GetHotels returns a slice of Hotels for the given hotelIDs and destinationID.
+	// If you do not want to filter by destinationID, set it to -1.
+	// If both are provided, only hotels for the destinationID are returned.
+	// If neither are provided, all hotels are returned.
+	// If there is no matching hotel, it returns an empty slice.
+	GetHotels(ctx context.Context, hotelIDs []string, destinationID int) ([]entity.Hotel, error)
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+type Handler struct {
+	hotelService Usecase
+}
+
+func NewHandler(hotels Usecase) *Handler {
+	return &Handler{
+		hotelService: hotels,
+	}
 }
 
 func (h *Handler) GetHotels(c *gin.Context) {
@@ -31,10 +56,29 @@ func (h *Handler) GetHotels(c *gin.Context) {
 		hotelIDs = strings.Split(hotels, ",")
 	}
 
-	fmt.Println("Hotels: ", hotelIDs)
-	fmt.Println("Destination: ", destination)
+	// parse the destination ID
+	var destinationID int
+	var err error
+	if destination != "" {
+		destinationID, err = strconv.Atoi(destination)
+		if err != nil {
+			c.AbortWithStatusJSON(400, gin.H{"error": ErrInvalidDestinationID})
+			return
+		}
+	} else {
+		destinationID = -1
+	}
 
-	c.JSON(200, gin.H{
-		"message": "Hotels retrieved successfully",
-	})
+	results, err := h.hotelService.GetHotels(c, hotelIDs, destinationID)
+	if err != nil {
+		c.AbortWithStatusJSON(500, gin.H{"error": ErrInternalServerError})
+		return
+	}
+
+	if len(results) == 0 {
+		c.AbortWithStatusJSON(404, gin.H{"error": ErrNoHotelsFound})
+		return
+	}
+
+	c.JSON(http.StatusOK, results)
 }
